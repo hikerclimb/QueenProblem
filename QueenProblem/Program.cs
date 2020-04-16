5181f89a4e77cd7222c60b7f0ded0e556dc28ca5
Aniket Karmarkar <akarmarkar@phillipcapital.com>
2020-04-15 19:55:05 -0500

    added ability to output grid

--- QueenProblem/Program.cs
+++ QueenProblem/Program.cs
@@ -10,16 +10,42 @@ namespace QueenProblem
     {
         static void Main(string[] args)
         {
+            int[,] array = { { 0,0,0,0,0,0},
+                            { 0,0,0,0,0,0},
+                            { 0,0,0,0,0,0},
+                            { 0,0,0,0,0,0},
+                            { 0,0,0,0,0,0},
+                            { 0,0,0,0,0,0}
+            };
+
             int[] firstArray = { 2, 4, 6, 1, 3, 5 }; //True
 
-            int[] secondArray = { 3, 4, 2, 1, 6, 5 }; //False
+            for(int index = 0; index <= firstArray.Length -1 ; index++)
+            {
+                //Console.WriteLine(index);
+                array[firstArray[index] - 1, index] = 1;
+            }
+
+            for (int row = 0; row <= firstArray.Length - 1; row++)
+            {
+                //Console.Write(row);
+                for (int col = firstArray.Length - 1; col >= 0 ; col--)
+                {
+                    Console.Write(array[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+            /*int[] secondArray = { 3, 4, 2, 1, 6, 5 }; //False
 
             int[] thirdArray = { 3, 5, 2, 3, 1, 5 };  //False
 
             Console.WriteLine(Check_If_No_Queen_Is_Present_On_Same_Row_Or_Position(firstArray));
             Console.WriteLine(Check_If_No_Queen_Is_Present_On_Same_Row_Or_Position(secondArray));
             Console.WriteLine(Check_If_No_Queen_Is_Present_On_Same_Row_Or_Position(thirdArray));
-            Console.ReadKey();
+            Console.ReadKey();*/
         }
 
         static bool Check_If_No_Queen_Is_Present_On_Same_Row_Or_Position(int[] array)
